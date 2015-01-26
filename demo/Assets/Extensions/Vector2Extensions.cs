a91f3fa350683cf433fde7038b88a8897b7413e0
Stafford Williams <stafford.williams@gmail.com>
2015-01-27 01:49:13 +1100

    texture generation

---
+++ demo/Assets/Extensions/Vector2Extensions.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector2Extensions
+{
+    public static void DrawLine(this Texture2D tex, int x0, int y0, int x1, int y1, Color col)
+    {
+        int dy = (int)(y1 - y0);
+        int dx = (int)(x1 - x0);
+        int stepx, stepy;
+
+        if (dy < 0) { dy = -dy; stepy = -1; }
+        else { stepy = 1; }
+        if (dx < 0) { dx = -dx; stepx = -1; }
+        else { stepx = 1; }
+        dy <<= 1;
+        dx <<= 1;
+
+        float fraction = 0;
+
+        tex.SetPixel(x0, y0, col);
+        if (dx > dy)
+        {
+            fraction = dy - (dx >> 1);
+            while (Mathf.Abs(x0 - x1) > 1)
+            {
+                if (fraction >= 0)
+                {
+                    y0 += stepy;
+                    fraction -= dx;
+                }
+                x0 += stepx;
+                fraction += dy;
+                tex.SetPixel(x0, y0, col);
+            }
+        }
+        else
+        {
+            fraction = dx - (dy >> 1);
+            while (Mathf.Abs(y0 - y1) > 1)
+            {
+                if (fraction >= 0)
+                {
+                    x0 += stepx;
+                    fraction -= dy;
+                }
+                y0 += stepy;
+                fraction += dx;
+                tex.SetPixel(x0, y0, col);
+            }
+        }
+    }
+
+    public struct Point
+    {
+        public short x;
+        public short y;
+        public Point(short aX, short aY) { x = aX; y = aY; }
+        public Point(int aX, int aY) : this((short)aX, (short)aY) { }
+    }
+
+    public static void FloodFillArea(this Texture2D aTex, int aX, int aY, Color aFillColor)
+    {
+        int w = aTex.width;
+        int h = aTex.height;
+        Color[] colors = aTex.GetPixels();
+        Color refCol = colors[aX + aY * w];
+        Queue<Point> nodes = new Queue<Point>();
+        nodes.Enqueue(new Point(aX, aY));
+        while (nodes.Count > 0)
+        {
+            Point current = nodes.Dequeue();
+            for (int i = current.x; i < w; i++)
+            {
+                Color C = colors[i + current.y * w];
+                if (C != refCol || C == aFillColor)
+                    break;
+                colors[i + current.y * w] = aFillColor;
+                if (current.y + 1 < h)
+                {
+                    C = colors[i + current.y * w + w];
+                    if (C == refCol && C != aFillColor)
+                        nodes.Enqueue(new Point(i, current.y + 1));
+                }
+                if (current.y - 1 >= 0)
+                {
+                    C = colors[i + current.y * w - w];
+                    if (C == refCol && C != aFillColor)
+                        nodes.Enqueue(new Point(i, current.y - 1));
+                }
+            }
+            for (int i = current.x - 1; i >= 0; i--)
+            {
+                Color C = colors[i + current.y * w];
+                if (C != refCol || C == aFillColor)
+                    break;
+                colors[i + current.y * w] = aFillColor;
+                if (current.y + 1 < h)
+                {
+                    C = colors[i + current.y * w + w];
+                    if (C == refCol && C != aFillColor)
+                        nodes.Enqueue(new Point(i, current.y + 1));
+                }
+                if (current.y - 1 >= 0)
+                {
+                    C = colors[i + current.y * w - w];
+                    if (C == refCol && C != aFillColor)
+                        nodes.Enqueue(new Point(i, current.y - 1));
+                }
+            }
+        }
+        aTex.SetPixels(colors);
+    }
+
+    public static void FloodFillBorder(this Texture2D aTex, int aX, int aY, Color aFillColor, Color aBorderColor)
+    {
+        int w = aTex.width;
+        int h = aTex.height;
+        Color[] colors = aTex.GetPixels();
+        byte[] checkedPixels = new byte[colors.Length];
+        Color refCol = aBorderColor;
+        Queue<Point> nodes = new Queue<Point>();
+        nodes.Enqueue(new Point(aX, aY));
+        while (nodes.Count > 0)
+        {
+            Point current = nodes.Dequeue();
+
+            for (int i = current.x; i < w; i++)
+            {
+                if (checkedPixels[i + current.y * w] > 0 || colors[i + current.y * w] == refCol)
+                    break;
+                colors[i + current.y * w] = aFillColor;
+                checkedPixels[i + current.y * w] = 1;
+                if (current.y + 1 < h)
+                {
+                    if (checkedPixels[i + current.y * w + w] == 0 && colors[i + current.y * w + w] != refCol)
+                        nodes.Enqueue(new Point(i, current.y + 1));
+                }
+                if (current.y - 1 >= 0)
+                {
+                    if (checkedPixels[i + current.y * w - w] == 0 && colors[i + current.y * w - w] != refCol)
+                        nodes.Enqueue(new Point(i, current.y - 1));
+                }
+            }
+            for (int i = current.x - 1; i >= 0; i--)
+            {
+                if (checkedPixels[i + current.y * w] > 0 || colors[i + current.y * w] == refCol)
+                    break;
+                colors[i + current.y * w] = aFillColor;
+                checkedPixels[i + current.y * w] = 1;
+                if (current.y + 1 < h)
+                {
+                    if (checkedPixels[i + current.y * w + w] == 0 && colors[i + current.y * w + w] != refCol)
+                        nodes.Enqueue(new Point(i, current.y + 1));
+                }
+                if (current.y - 1 >= 0)
+                {
+                    if (checkedPixels[i + current.y * w - w] == 0 && colors[i + current.y * w - w] != refCol)
+                        nodes.Enqueue(new Point(i, current.y - 1));
+                }
+            }
+        }
+        aTex.SetPixels(colors);
+    }
+}
+
--- demo/Assets/VoronoiDemo.cs
+++ demo/Assets/VoronoiDemo.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 using System.Collections.Generic;
 using Delaunay;
 using Delaunay.Geo;
+using System.Linq;
 
 public class VoronoiDemo : MonoBehaviour
 {
-	[SerializeField]
-	private int
-		m_pointCount = 300;
+	private int _pointCount = 50;
 
 	private List<Vector2> m_points;
-	private float m_mapWidth = 100;
-	private float m_mapHeight = 50;
-	private List<LineSegment> m_edges = null;
-	private List<LineSegment> m_spanningTree;
-	private List<LineSegment> m_delaunayTriangulation;
+	const float _mapWidth = 50;
+	const float _mapHeight = 50;
+	private List<LineSegment> _edges = null;
+	private List<LineSegment> _spanningTree;
+	private List<LineSegment> _delaunayTriangulation;
+
+    const int _textureScale = 5;
+    int _textureWidth = (int)_mapWidth * _textureScale;
+    int _textureHeight = (int)_mapHeight * _textureScale;
+
+    const int NUM_LLOYD_RELAXATIONS = 2;
 
 	void Awake ()
 	{
@@ -34,19 +39,73 @@ public class VoronoiDemo : MonoBehaviour
 		List<uint> colors = new List<uint> ();
 		m_points = new List<Vector2> ();
 			
-		for (int i = 0; i < m_pointCount; i++) {
+		for (int i = 0; i < _pointCount; i++) {
 			colors.Add (0);
 			m_points.Add (new Vector2 (
-					UnityEngine.Random.Range (0, m_mapWidth),
-					UnityEngine.Random.Range (0, m_mapHeight))
+					UnityEngine.Random.Range (0, _mapWidth),
+					UnityEngine.Random.Range (0, _mapHeight))
 			);
 		}
-		Delaunay.Voronoi v = new Delaunay.Voronoi (m_points, colors, new Rect (0, 0, m_mapWidth, m_mapHeight));
-		m_edges = v.VoronoiDiagram ();
+
+        for (int i = 0; i < NUM_LLOYD_RELAXATIONS; i++)
+        {
+            m_points = RelaxPoints(m_points).ToList();
+        }
+
+		Delaunay.Voronoi v = new Delaunay.Voronoi (m_points, colors, new Rect (0, 0, _mapWidth, _mapHeight));
+		_edges = v.VoronoiDiagram ();
 			
-		m_spanningTree = v.SpanningTree (KruskalType.MINIMUM);
-		m_delaunayTriangulation = v.DelaunayTriangulation ();
+		_spanningTree = v.SpanningTree (KruskalType.MINIMUM);
+		_delaunayTriangulation = v.DelaunayTriangulation ();
+
+        CreateTexture();
 	}
+
+    private void CreateTexture()
+    {
+        Texture2D texture = new Texture2D(_textureWidth, _textureHeight);
+        texture.SetPixels(Enumerable.Repeat(Color.magenta, _textureWidth * _textureHeight).ToArray());
+
+        var lines = _edges.Select(p => new[] 
+        { 
+            ((Vector2)(p.p0)).x, ((Vector2)(p.p0)).y,
+            ((Vector2)(p.p1)).x, ((Vector2)(p.p1)).y
+        }).ToArray();
+
+        foreach (var line in lines)
+        {
+            DrawLine(texture, line[0], line[1], line[2], line[3], Color.black);
+        }
+
+        //DrawLine(texture, 0, 0, _mapWidth * _textureScale, _mapHeight * _textureScale, Color.yellow);
+        texture.FloodFillArea(2 * _textureScale, 2 * _textureScale, Color.red);
+        texture.FloodFillArea((int)(_mapWidth / 2 * _textureScale), (int)(_mapHeight / 2 * _textureScale), Color.blue);
+        texture.Apply();
+        
+        var plane = GameObject.Find("Background");
+        plane.renderer.material.mainTexture = texture;
+        plane.transform.localPosition = new Vector3(_mapWidth / 2, _mapHeight / 2, 1);
+    }
+
+    private void DrawLine(Texture2D texture, float x0, float y0, float x1, float y1, Color color)
+    {
+        texture.DrawLine((int)(x0 * _textureScale), (int)(y0 * _textureScale), (int)(x1 * _textureScale), (int)(y1 * _textureScale), color);
+    }
+
+    private IEnumerable<Vector2> RelaxPoints(IEnumerable<Vector2> startingPoints)
+    {        
+        Delaunay.Voronoi v = new Delaunay.Voronoi(startingPoints.ToList(), null, new Rect(0, 0, _mapWidth, _mapHeight));
+        foreach (var point in startingPoints)
+        {
+            var region = v.Region(point);
+            point.Set(0, 0);
+            foreach (var r in region)
+                point.Set(point.x + r.x, point.y + r.y);
+
+            point.Set(point.x / region.Count, point.y / region.Count);
+            yield return point;
+        }
+    }
 
 	void OnDrawGizmos ()
 	{
@@ -57,38 +116,19 @@ public class VoronoiDemo : MonoBehaviour
 			}
 		}
 
-		if (m_edges != null) {
-			Gizmos.color = Color.white;
-			for (int i = 0; i< m_edges.Count; i++) {
-				Vector2 left = (Vector2)m_edges [i].p0;
-				Vector2 right = (Vector2)m_edges [i].p1;
-				Gizmos.DrawLine ((Vector3)left, (Vector3)right);
-			}
-		}
-
-		Gizmos.color = Color.magenta;
-		if (m_delaunayTriangulation != null) {
-			for (int i = 0; i< m_delaunayTriangulation.Count; i++) {
-				Vector2 left = (Vector2)m_delaunayTriangulation [i].p0;
-				Vector2 right = (Vector2)m_delaunayTriangulation [i].p1;
-				Gizmos.DrawLine ((Vector3)left, (Vector3)right);
-			}
-		}
-
-		if (m_spanningTree != null) {
-			Gizmos.color = Color.green;
-			for (int i = 0; i< m_spanningTree.Count; i++) {
-				LineSegment seg = m_spanningTree [i];				
-				Vector2 left = (Vector2)seg.p0;
-				Vector2 right = (Vector2)seg.p1;
-				Gizmos.DrawLine ((Vector3)left, (Vector3)right);
-			}
-		}
+        //if (_edges != null) {
+        //    Gizmos.color = Color.white;
+        //    for (int i = 0; i< _edges.Count; i++) {
+        //        Vector2 left = (Vector2)_edges [i].p0;
+        //        Vector2 right = (Vector2)_edges [i].p1;
+        //        Gizmos.DrawLine ((Vector3)left, (Vector3)right);
+        //    }
+        //}
 
 		Gizmos.color = Color.yellow;
-		Gizmos.DrawLine (new Vector2 (0, 0), new Vector2 (0, m_mapHeight));
-		Gizmos.DrawLine (new Vector2 (0, 0), new Vector2 (m_mapWidth, 0));
-		Gizmos.DrawLine (new Vector2 (m_mapWidth, 0), new Vector2 (m_mapWidth, m_mapHeight));
-		Gizmos.DrawLine (new Vector2 (0, m_mapHeight), new Vector2 (m_mapWidth, m_mapHeight));
+		Gizmos.DrawLine (new Vector2 (0, 0), new Vector2 (0, _mapHeight));
+		Gizmos.DrawLine (new Vector2 (0, 0), new Vector2 (_mapWidth, 0));
+		Gizmos.DrawLine (new Vector2 (_mapWidth, 0), new Vector2 (_mapWidth, _mapHeight));
+		Gizmos.DrawLine (new Vector2 (0, _mapHeight), new Vector2 (_mapWidth, _mapHeight));
 	}
 }
