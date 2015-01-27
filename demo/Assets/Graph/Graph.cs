dfc677aea5b123a504ddeccb9d8f6f610610bc40
Stafford Williams <stafford.williams@gmail.com>
2015-01-27 22:54:34 +1100

    assign corner elevations

--- demo/Assets/Graph/Graph.cs
+++ demo/Assets/Graph/Graph.cs
@@ -9,6 +9,8 @@ namespace Assets.Graph
     public class Graph
     {
         List<KeyValuePair<int, Corner>> _cornerMap = new List<KeyValuePair<int, Corner>>();
+        Func<Vector2, bool> inside;
+        bool _needsMoreRandomness;
 
         public int Width { get; private set; }
         public int Height { get; private set; }
@@ -20,8 +22,23 @@ namespace Assets.Graph
         {
             Width = width;
             Height = height;
+            inside = IslandShape.makeRadial();
 
-            //var p:Center, q:Corner, point:Point, other:Point;
+            BuildGraph(points, voronoi);
+
+            AssignCornerElevations();
+        }
+
+        // Build graph data structure in 'edges', 'centers', 'corners',
+        // based on information in the Voronoi results: point.neighbors
+        // will be a list of neighboring points of the same type (corner
+        // or center); point.edges will be a list of edges that include
+        // that point. Each edge connects to four points: the Voronoi edge
+        // edge.{v0,v1} and its dual Delaunay triangle edge edge.{d0,d1}.
+        // For boundary polygons, the Delaunay edge will have one null
+        // point, and the Voronoi edge may be null.
+        private void BuildGraph(IEnumerable<Vector2> points, Delaunay.Voronoi voronoi)
+        {
             var libedges = voronoi.Edges();
 
             var centerLookup = new Dictionary<Vector2?, Center>();
@@ -144,16 +161,87 @@ namespace Assets.Graph
             return corner;
         }
 
-        // Helper functions for the following for loop; ideally these
-        // would be inlined
         private void addToCornerList(List<Corner> v, Corner x)
         {
             if (x != null && v.IndexOf(x) < 0)
                 v.Add(x);
         }
+
         private void addToCenterList(List<Center> v, Center x)
         {
             if (x != null && v.IndexOf(x) < 0) { v.Add(x); }
+        }
+
+        // Determine elevations and water at Voronoi corners. By
+        // construction, we have no local minima. This is important for
+        // the downslope vectors later, which are used in the river
+        // construction algorithm. Also by construction, inlets/bays
+        // push low elevation areas inland, which means many rivers end
+        // up flowing out through them. Also by construction, lakes
+        // often end up on river paths because they don't raise the
+        // elevation as much as other terrain does.
+        private void AssignCornerElevations()
+        {
+            //var q:Corner, s:Corner;
+            var queue = new Queue<Corner>();
+
+            foreach (var q in corners)
+            {
+                q.water = !inside(q.point);
+            }
+
+            foreach (var q in corners)
+            {
+                // The edges of the map are elevation 0
+                if (q.border)
+                {
+                    q.elevation = 0;
+                    queue.Enqueue(q);
+                }
+                else
+                {
+                    q.elevation = float.PositiveInfinity;
+                }
+            }
+
+            // Traverse the graph and assign elevations to each point. As we
+            // move away from the map border, increase the elevations. This
+            // guarantees that rivers always have a way down to the coast by
+            // going downhill (no local minima).
+            while (queue.Any())
+            {
+                var q = queue.Dequeue();
+
+                foreach (var s in q.adjacent)
+                {
+                    // Every step up is epsilon over water or 1 over land. The
+                    // number doesn't matter because we'll rescale the
+                    // elevations later.
+                    var newElevation = 0.01f + q.elevation;
+                    if (!q.water && !s.water)
+                    {
+                        newElevation += 1;
+                        if (_needsMoreRandomness)
+                        {
+                            // HACK: the map looks nice because of randomness of
+                            // points, randomness of rivers, and randomness of
+                            // edges. Without random point selection, I needed to
+                            // inject some more randomness to make maps look
+                            // nicer. I'm doing it here, with elevations, but I
+                            // think there must be a better way. This hack is only
+                            // used with square/hexagon grids.
+                            newElevation += UnityEngine.Random.value; ;
+                        }
+                    }
+                    // If this point changed, we'll add it to the queue so
+                    // that we can process its neighbors too.
+                    if (newElevation < s.elevation)
+                    {
+                        s.elevation = newElevation;
+                        queue.Enqueue(s);
+                    }
+                }
+            }
         }
     }
 }
---
+++ demo/Assets/Graph/IslandShape.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Graph
+{
+    public class IslandShape
+    {
+        // This class has factory functions for generating islands of
+        // different shapes. The factory returns a function that takes a
+        // normalized point (x and y are -1 to +1) and returns true if the
+        // point should be on the island, and false if it should be water
+        // (lake or ocean).
+
+        // The radial island radius is based on overlapping sine waves 
+        public static float ISLAND_FACTOR = 1.07f;  // 1.0 means no small islands; 2.0 leads to a lot
+        public static System.Func<Vector2, bool> makeRadial()
+        {
+            var bumps = Random.Range(1, 6);
+            var startAngle = Random.value * 2 * Mathf.PI;
+            var dipAngle = Random.value * 2 * Mathf.PI;
+
+            var random = Random.value;
+            var start = 0.2f;
+            var end = 0.7f;
+
+            var dipWidth = (end - start) * random + start;
+
+            System.Func<Vector2, bool> inside = q =>
+            {
+                var angle = Mathf.Atan2(q.y, q.x);
+                var length = 0.5 * (Mathf.Max(Mathf.Abs(q.x), Mathf.Abs(q.y)) + q.magnitude);
+
+                var r1 = 0.5 + 0.40 * Mathf.Sin(startAngle + bumps * angle + Mathf.Cos((bumps + 3) * angle));
+                var r2 = 0.7 - 0.20 * Mathf.Sin(startAngle + bumps * angle - Mathf.Sin((bumps + 2) * angle));
+                if (Mathf.Abs(angle - dipAngle) < dipWidth
+                    || Mathf.Abs(angle - dipAngle + 2 * Mathf.PI) < dipWidth
+                    || Mathf.Abs(angle - dipAngle - 2 * Mathf.PI) < dipWidth)
+                {
+                    r1 = r2 = 0.2;
+                }
+                return (length < r1 || (length > r1 * ISLAND_FACTOR && length < r2));
+            };
+
+            return inside;
+        }
+
+        // The Perlin-based island combines perlin noise with the radius
+        public static System.Func<Vector2, bool> makePerlin()
+        {
+            var offset = Random.Range(0, int.MaxValue / 2);
+            System.Func<Vector2, bool> inside = q =>
+            {
+                return Mathf.PerlinNoise(q.x, q.y) > (0.3 + 0.3 * q.magnitude * q.magnitude);
+            };
+            return inside;
+        }
+
+        // The square shape fills the entire space with land
+        public static System.Func<Vector2, bool> makeSquare()
+        {
+            System.Func<Vector2, bool> inside = q => { return true; };
+            return inside;
+        }
+    }
+}
